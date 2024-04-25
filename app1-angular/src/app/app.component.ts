5555fd60973abaae8246794cea2ddb052737e561
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-04-25 10:07:42 -0500

    Actualización xd

--- app1-angular/src/app/app.component.ts
+++ app1-angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { response } from 'express';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,21 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'app1-angular'
   titulo: string = "Lista Productos desde Angular";
+  productos: any[] = []
+  constructor() {
+    this.listarProductos()
+  }
+  listarProductos() {
+    fetch('http://127.0.0.1:3000/producto')
+      .then(response => response.json())
+      .then(json => {
+        this.productos = json
+      })
+  }
+
 }
+
+
+
+
+
