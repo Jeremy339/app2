0c883c1e2238e0da84bd815fba01742377243aa5
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-04-26 08:29:36 -0500

    Solucionado Angular

--- app1-angular/src/app/app.component.ts
+++ app1-angular/src/app/app.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { response } from 'express';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app1-angular'
-  titulo: string = "Lista Productos desde Angular";
-  productos: any[] = []
-  constructor() {
+  
+  title = 'app1-angular';
+  titulo: string ="Lista de productos desde Angular";
+  productos: any [] = []
+  constructor(){
     this.listarProductos()
   }
-  listarProductos() {
-    fetch('http://127.0.0.1:3000/producto')
-      .then(response => response.json())
-      .then(json => {
-        this.productos = json
-      })
-  }
 
+  listarProductos(){
+    //solicitando datos al proyecto de NestJS desde Angular
+    fetch('http://127.0.0.1:3000/producto')
+    .then(response=> response.json())
+    .then(json=> {
+      this.productos=json
+    })
+  }
 }
 
 
