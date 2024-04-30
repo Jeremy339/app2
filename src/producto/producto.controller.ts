20b19feb20566284a14497f78e9d3e99c78d0db2
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-04-30 08:33:35 -0500

    Arquitectura

---
+++ src/producto/producto.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+@Controller('producto')
+export class ProductoController {
+
+    @Get()
+    funListar(){
+      return["Teclado", "Monitor", "Ratón"]
+    }
+    @Post()
+    funGuardar(@Body()prod){
+      return prod
+    }
+
+    @Get(':id')
+    funMostrar(@Param('id')id){
+        return "Mostrando...." +id;
+    }
+    
+    @Put(':id')
+    funModificar(@Param('id')id,@Body()prod){
+        return "Modificando" +id;
+    }
+
+    @Delete(':id')
+    funEliminar(@Param('id')id){
+        return "Eliminando" +id;
+    }
+}
+
+
